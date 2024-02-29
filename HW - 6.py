4307724f3d692d04e90230c88c9ae3e069e86004
OlenaBogoliubova <143827030+OlenaBogoliubova@users.noreply.github.com>
2024-02-29 13:55:34 +0100

    Add files via upload

--- HW - 6.py
+++ HW - 6.py
@@ -1,5 +1,48 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
+
+def dfs_iterative(graph, start_vertex):
+    visited = set()
+    stack = [start_vertex]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.add(vertex)
+            stack.extend(reversed(graph[vertex]))
+
+
+def bfs_iterative(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=" ")
+            visited.add(vertex)
+            queue.extend(set(graph[vertex]) - visited)
+
+    return visited
+
+
+def dijkstra_algorithm(graph, start_node):
+    distances = {node: float('inf') for node in graph.nodes}
+    distances[start_node] = 0
+    visited = set()
+
+    while len(visited) < len(graph.nodes):
+        current_node = min(
+            (node for node in graph.nodes if node not in visited), key=lambda x: distances[x])
+        visited.add(current_node)
+
+        for neighbor, weight in graph[current_node].items():
+            potential_distance = distances[current_node] + weight['weight']
+            if potential_distance < distances[neighbor]:
+                distances[neighbor] = potential_distance
+
+    return distances
 
 # Створення графа "Штучна нейронна мережа"
 G = nx.DiGraph()
